d429b8ab5d3c0f50386559b623d47d1d0ff42494
Neeraj Pandey <68136890+pandey1999@users.noreply.github.com>
2023-07-25 10:42:12 +0530

    Make server by using nodejs and make routing

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const { log } = require("console");
-const http =require("http");
+const http = require("http");
 
-//create the server 
-const server = http.createServer(async(req,res)=>{
-    res.writeHead(200,{'Content-type':'text-plain'});
-    res.write("nodejs server")
-    res.end()
-})
+//create the server
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const header = req.header;
 
-const PORT=4000
-server.listen(PORT,()=>{
-    console.log(`Server is listening on PORT ${PORT}`);
-})
+  //Now we have to go for different routes
+
+  if (url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello from Homepage");
+  } else if (url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("This is About page");
+  } else if (url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("This is About page");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("No Page found");
+  }
+});
+
+const PORT = 4000;
+server.listen(PORT, () => {
+  console.log(`server is listening on PORT ${PORT}`);
+});
