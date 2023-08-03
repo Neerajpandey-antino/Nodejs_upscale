2c13cc8cb39d1d3a527619fa6110d6bf14f66e47
Neeraj Pandey <68136890+pandey1999@users.noreply.github.com>
2023-08-03 09:34:04 +0530

    Using axios and fetch data from third party

--- index.js
+++ index.js
@@ -1,56 +1,27 @@
+// server.js
 const express = require('express');
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
+const axios = require('axios');
+
 const app = express();
-const {sendMail} = require("../Nodejs_upscale/sendmail")
-const port = 4000;
+const PORT = 4000;
 
-// Replace these values with your email configuration
-const emailConfig = {
-  service: 'YourEmailServiceProvider',
-  auth: {
-    user: 'YourEmailAddress',
-    pass: 'YourEmailPassword'
+app.get('/products', async (req, res) => {
+  try {
+    // Fetch product data from the JSONPlaceholder API
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+
+    // Extract the product data from the response
+    const products = response.data;
+
+    // Send the product data as the API response
+    res.json(products);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch product data' });
   }
-};
-
-const transporter = nodemailer.createTransport(emailConfig);
-
-app.use(bodyParser.json());
-
-// Simulate a user database (replace this with your actual database logic)
-const users = [
-  { id: 1, email: 'user1@example.com', name: 'User 1' },
-  { id: 2, email: 'user2@example.com', name: 'User 2' }
-];
-
-// Forgot Password API
-app.post('/forgot-password', (req, res) => {
-  const { email } = req.body;
-  const user = users.find((user) => user.email === email);
-
-  if (!user) {
-    return res.status(404).json({ message: 'User not found' });
-  }
-
-  const mailOptions = {
-    from: 'YourEmailAddress',
-    to: user.email,
-    subject: 'Password Reset Request',
-    text: `Hi ${user.name},\n\nPlease click on the link below to reset your password.\n\nhttps://yourdomain.com/reset-password?userId=${user.id}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error(error);
-      return res.status(500).json({ message: 'Something went wrong while sending the email' });
-    }
-    console.log('Email sent: ' + info.response);
-    res.json({ message: 'Email sent successfully' });
-  });
 });
 
-// Run the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
